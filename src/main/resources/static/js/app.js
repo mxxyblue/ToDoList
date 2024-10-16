bc8866c49cf599c70657b454dd0d2cb2f4015ccd
mxxyblue <51143943+mxxyblue@users.noreply.github.com>
2024-10-16 21:14:27 +0900

    Update app.js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -7,16 +7,16 @@ window.onload = function(){
 function addTask(){ //add 버튼 클릭 이벤트
 	let taskInput = document.querySelector('#task-input').value; //input 값 가져오기
 	let parentNode = document.querySelector('#task-list'); 
-	let newElement = createTaskDiv(taskInput); //새 div 생성
+	let newElement = createNewTask(taskInput); //새 div 생성
 	
 	parentNode.appendChild(newElement); //task-list에 child 추가하기
 }
 
-function createTaskDiv(taskInput){
+function createNewTask(taskInput){
 	let newText = createNewText(taskInput);
-	let newButton = createNewButton();
+	let newButton = createDeleteButton();
 	
-	return createNewDiv(newText, newButton);
+	return createNewToDoList(newText, newButton);
 }
 
 function createNewText(taskInput){ //새 텍스트 요소 생성
@@ -26,7 +26,7 @@ function createNewText(taskInput){ //새 텍스트 요소 생성
 	return newText;
 }
 
-function createNewButton(){ //새 버튼 요소 생성
+function createDeleteButton(){ //새 삭제 버튼 생성
 	let newButton = document.createElement("button"); 
 	newButton.innerHTML = "삭제";
 	newButton.addEventListener('click', deleteTask); //삭제 버튼에 이벤트 추가
@@ -34,8 +34,8 @@ function createNewButton(){ //새 버튼 요소 생성
 	return newButton;
 }
 
-function createNewDiv(newText, newButton){ //새 div에 버튼, 텍스트 요소 추가
-	let newElement = document.createElement("div");  //const ? let ? 
+function createNewToDoList(newText, newButton){ //새 div에 버튼, 텍스트 요소 추가
+	let newElement = document.createElement("div"); 
 	newElement.setAttribute('class', 'task-item');
 	newElement.appendChild(newText);
 	newElement.appendChild(newButton);
